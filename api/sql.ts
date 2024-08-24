8328c2e04792d471ec61f04b3e9c0ecf263ac58a
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-08-24 16:09:19 -0400

    sql list

---
+++ api/sql.ts
@@ -0,0 +1,35 @@
+import * as SQLite from "expo-sqlite";
+
+let db: SQLite.SQLiteDatabase;
+
+export const initDatabase = async () => {
+  if (db === null) db = await SQLite.openDatabaseAsync("todolist.db");
+  return db;
+};
+
+export const createListTable = async () => {
+  await db.execAsync(`
+    CREATE TABLE IF NOT EXISTS lists (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL, description TEXT NOT NULL, length INTEGER NOT NULL, emoji TEXT NOT NULL, type TEXT NOT NULL);
+  `);
+};
+
+export const createList = async (
+  title: string,
+  description: string,
+  length: number,
+  emoji: string,
+  type: string
+) => {
+  return db.runAsync(
+    "INSERT INTO lists (title, description, length, emoji, type) VALUES (?, ?, ?, ?, ?)",
+    title,
+    description,
+    length,
+    emoji,
+    type
+  );
+};
+
+export const getList = async (id: number) => {
+  return db.getFirstAsync("SELECT * FROM lists WHERE id = ?", id);
+};
--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,8 +1,8 @@
 export interface List {
   id: number;
   title: string;
-  description?: string;
-  length?: number;
-  emoji?: string;
-  type?: string;
+  description: string;
+  length: number;
+  emoji: string;
+  type: string;
 }
