850eff83a4ed9439d95e6aa906aa5dee0e3e8d0c
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-08-24 17:58:50 -0400

    list card routing

---
+++ app/list/[id].tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Post = () => {
+  return (
+    <SafeAreaView>
+      <Text>Post</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Post;
--- components/ListCard.tsx
+++ components/ListCard.tsx
@@ -1,12 +1,18 @@
 import { List } from "@/interfaces";
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { Pressable, Text, View } from "react-native";
 
 const ListCard = ({ item }: { item: List }) => {
   return (
-    <View className="w-36 h-24 border rounded-md">
+    <Pressable
+      className="w-36 h-24 border shadow-sm rounded-md bg-zinc-200 active:opacity-70 hover:opacity-70"
+      onPress={() => {
+        router.push(`/list/${item.id}`);
+      }}
+    >
       <Text className="text-lg text-center">{item.title}</Text>
       <Text className="text-sm text-center">{item.length}</Text>
-    </View>
+    </Pressable>
   );
 };
 
